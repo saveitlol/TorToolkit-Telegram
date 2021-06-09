0a061b2c7f95b4a446c410c3dd105cd6f94072a4
Devill Don <53294521+devillD@users.noreply.github.com>
2021-06-09 21:15:55 +0600

    Changed value

--- alive.sh
+++ alive.sh
@@ -1,6 +1,6 @@
 while true
 do
-    sleep 60 #sleep NUMBER[SUFFIX]. SUFFIX= seconds (s), minutes (m), hours (h) and days (d). Given value 5m is 5 minutes.
+    sleep 5 #sleep NUMBER[SUFFIX]. SUFFIX= seconds (s), minutes (m), hours (h) and days (d). Given value 5 is 5 seconds.
     echo "Preventing site from idling..."
     wget -q -O/dev/null $BASE_URL_OF_BOT
 done
