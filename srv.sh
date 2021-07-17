9d6e291d1cb6ae363f8573383e1c1bdaeae9d769
Devill Don <53294521+devillD@users.noreply.github.com>
2021-07-17 09:30:58 +0600

    Rclone flugs

--- srv.sh
+++ srv.sh
@@ -2,3 +2,4 @@ wget $SAFILE -O sa.json && \
 wget $CONFIG -O .config/rclone/rclone.conf && \
 rclone serve http $NAME: --addr :$PORT --vfs-read-chunk-size 256M \
                                        --user $USER --pass $PASS
+# For more flugs: https://rclone.org/flags/
